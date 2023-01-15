a159453f45f9be0e5d0d47f0fb249af59f376cc3
Harbir Singh <harbir066@gmail.com>
2023-01-15 19:52:51 +0530

    feat : add function to find longest string

---
+++ jsQuestions/Q4.js
@@ -0,0 +1,31 @@
+function findLongestString(stringArray){
+    let longestString=stringArray[0];
+    let maxLength=stringArray[0].length 
+    stringArray.forEach(string => {
+        if(string.length > maxLength){
+            longestString=string;
+            maxLength=maxLength;
+        }
+    });
+    return longestString;
+}
+
+// console.log(findLongestString(["we", "love", "code", "academy"]))
+// console.log(findLongestString(["sd",'a']))
+
+const findLongestString2 = (stringArray) => {
+    let longestString=stringArray[0];
+    let maxLength=stringArray[0].length 
+    stringArray.forEach(string => {
+        if(string.length > maxLength){
+            longestString=string;
+            maxLength=maxLength;
+        }
+    });
+    return longestString;
+}
+
+// console.log(findLongestString2(["we", "love", "code", "academy"]))
+// console.log(findLongestString2(["sd",'a']))
+
+module.exports = {findLongestString,findLongestString2}
