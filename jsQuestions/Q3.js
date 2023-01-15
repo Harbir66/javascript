e4568c4f765cf1eb3e657f71e8da7d9c9aaff448
Harbir Singh <harbir066@gmail.com>
2023-01-15 19:46:03 +0530

    feat : add function to change capitalization of the string

---
+++ jsQuestions/Q3.js
@@ -0,0 +1,27 @@
+function changeCapitalization(string){
+    const array = string.split("");
+    newString="";
+    array.forEach(char => {
+        newString+=(char.charCodeAt(0)>= "a".charCodeAt(0) )?char.toUpperCase():char.toLowerCase();
+        
+    });
+    return newString;
+}
+
+// console.log(changeCapitalization("MeRRy hAD a LITTle lAMp"))
+// console.log(changeCapitalization("FOSID dasdf NDdsDfdssdSD"))
+
+const changeCapitalization2 = (string) =>{
+    const array = string.split("");
+    newString="";
+    array.forEach(char => {
+        newString+=(char.charCodeAt(0)>= "a".charCodeAt(0) )?char.toUpperCase():char.toLowerCase();
+        
+    });
+    return newString;
+}
+
+// console.log(changeCapitalization2("MeRRy hAD a LITTle lAMp"))
+// console.log(changeCapitalization2("FOSID dasdf NDdsDfdssdSD"))
+
+module.exports = {changeCapitalization,changeCapitalization2};
