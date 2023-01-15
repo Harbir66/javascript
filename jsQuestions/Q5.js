2cf486b518f12a3a8bd68d2b620f2e203cb2b9c6
Harbir Singh <harbir066@gmail.com>
2023-01-15 19:57:24 +0530

    feat : add function to check leap year

---
+++ jsQuestions/Q5.js
@@ -0,0 +1,15 @@
+function isLeapYear(year){
+    return (year%400==0) || (year%4==0 && year%100!=0);
+}
+
+// console.log(isLeapYear(2000))
+// console.log(isLeapYear(2100))
+// console.log(isLeapYear(2400))
+
+const isLeapYear2 = year => (year%400==0) || (year%4==0 && year%100 !=0);
+
+// console.log(isLeapYear2(2000))
+// console.log(isLeapYear2(2100))
+// console.log(isLeapYear2(2400))
+
+module.exports = {isLeapYear,isLeapYear2}
